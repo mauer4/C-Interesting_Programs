4114710276d07a3e88d4a740746c2a2d19db365a
Adin Mauer <mauer4@gmail.com>
2022-08-02 12:23:11 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// LIST OF FUNCTIONS:
+// 1. Power of 2
+// 2. Prime Numbers
+// 3. Roman Letters
+// 4. Day of the Week
+// 5. Calendar
+// 6. Memory-Swap
+// 7. Blobk Structure
+// 8. Swap Macro using Block Structure
+// 9. printf with %b
+
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
